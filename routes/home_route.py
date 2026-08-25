be05ce8d50f62ac660a0b779ad3e0ad6d107e6c9
aashirzubair999 <aashirzubair84@gmail.com>
2026-08-25 12:20:20 +0500

    Version to 2.0.0

--- routes/home_route.py
+++ routes/home_route.py
@@ -12,7 +12,7 @@ def home():
     
     return {
         "message": "Palapalai is working perfectly!",
-        "version": "1.0.2",
+        "version": "2.0.0",
         "status": "success"
     }
 
