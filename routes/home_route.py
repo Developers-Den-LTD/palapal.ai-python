3b8df32ea972d300c4bfbe0ff8dce4933b36a72d
aashirzubair999 <aashirzubair84@gmail.com>
2026-09-08 13:40:47 +0500

    Dummy push

--- routes/home_route.py
+++ routes/home_route.py
@@ -12,7 +12,7 @@ def home():
     
     return {
         "message": "Palapalai is working perfectly!",
-        "version": "2.0.0",
+        "version": "2.0.1",
         "status": "success"
     }
 
